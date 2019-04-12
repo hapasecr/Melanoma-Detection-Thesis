949473f3c21bea6e7fbaa20b83e9a106903dc6a5
Chaitanya Hapase <hapasecr@Chaitanyas-MacBook-Pro.local>
2019-04-12 05:15:23 -0500

    Changed init.py for 'app not found'

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from config import Config
 from werkzeug.utils import secure_filename
-from app import routes
 
 UPLOAD_FOLDER = './ip'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+from app import routes
 
 bootstrap = Bootstrap(app)
 
